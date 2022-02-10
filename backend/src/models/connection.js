b37fda570a44cbe6906c16bf593f37dac2ea2a6e
Wellington <junior.wel02@exemplo.br>
2022-02-10 16:59:05 -0300

    connection

---
+++ backend/src/models/connection.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+const { MongoClient } = require("mongodb");
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const MONGO_DB_URL = `mongodb://${
+  process.env.HOST || "mongodb"
+}:27017/tasks`;
+
+const DB_NAME = "tasks";
+
+let db = null;
+
+function connection() {
+  return db
+    ? Promise.resolve(db)
+    : MongoClient.connect(MONGO_DB_URL, OPTIONS).then((conn) => {
+        db = conn.db(DB_NAME);
+        return db;
+      });
+}
+
+module.exports = connection;
